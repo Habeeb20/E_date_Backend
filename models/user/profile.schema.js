38166e3342e11a69d25acc3098d8a6d00d887cb5
Habeeb20 <Ademolawaliyu1@gmail.com>
2025-02-26 17:01:40 +0100

    commit

--- models/user/profile.schema.js
+++ models/user/profile.schema.js
@@ -1,62 +1,68 @@
 import mongoose from "mongoose";
 import slugify from "slugify";
-const profileSchema = new mongoose.Schema({
-
-    userId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+const profileSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
     userEmail: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    
-      },
-    firstName:{
-        type:String,
-        required: true
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-    lastName:{
-        type:String,
-        required: true
+    firstName: {
+      type: String,
+      required: true,
     },
-    dateOfBirth:{
-        type:Date,
-        required: true
+    lastName: {
+      type: String,
+      required: true,
     },
-    gender:{
-        type:String,
-        required: true
+    dateOfBirth: {
+      type: Date,
+      required: true,
     },
-    maritalStatus:{
-        type:String,
-        required: true
+    gender: {
+      type: String,
+      required: true,
     },
-    interest:[{type:String }],
-    nationality:{
-        type:String,
-        required: true
+    maritalStatus: {
+      type: String,
+      required: true,
     },
-    ProfilePicture:{
-        type:String
+    interest: [{ type: String }],
+    nationality: {
+      type: String,
+      required: true,
     },
-    skinColor:{
-        type:String,
+    ProfilePicture: {
+      type: String,
     },
-    EyeColor:{
-        type:String
-    }
-}, {timestamps: true})
+    skinColor: {
+      type: String,
+    },
+    EyeColor: {
+      type: String,
+    },
 
+    slug: {
+      type: String,
+      unique: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
 
-profileSchema.pre("save", function(next){
-    if(!this.slug){
-      this.slug = slugify(this.firstName, { lower: true, strict: true });
-    }
-    next();
-  })
+profileSchema.pre("save", function (next) {
+  if (!this.slug) {
+    this.slug = slugify(this.firstName, { lower: true, strict: true });
+  }
+  next();
+});
 
-  const Profile = mongoose.model("Profile", profileSchema)
-  
-export default Profile
+const Profile = mongoose.model("Profile", profileSchema);
+
+export default Profile;
