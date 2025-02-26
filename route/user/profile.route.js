1f28795bc65469a67ef47ac3277cd6e7a461f2fe
Habeeb20 <Ademolawaliyu1@gmail.com>
2025-02-26 16:30:06 +0100

    commit

--- route/user/profile.route.js
+++ route/user/profile.route.js
@@ -14,7 +14,7 @@ profilerouter.post("/createprofile", async (req, res) => {
             return res.status(400).json({ message: "Email is required" });
         }
 
-        const user = await User.findOne({ userEmail });
+        const user = await User.findOne({ email: userEmail });
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
